8b80d22d625d45206c364c2255ad228d86fc81cc
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2026-05-25 15:13:22 -0400

    fix(QueryClient): Configurar staleTime, gcTime y refetchOnWindowFocus

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,16 @@ import { AuthProvider } from "./context/AuthContext";
 import { router } from "@/routes/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryclient = new QueryClient();
+const queryclient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 2 * 60 * 1000, // 2 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+      refetchOnWindowFocus: true,
+      retry: 1,
+    }
+  }
+});
 
 export default function App() {
   return (
